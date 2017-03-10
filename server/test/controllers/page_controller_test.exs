327691cd54c265e74669ddb8c40a66b552a0634b
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-03-10 16:31:34 +0100

    Playing around with tests

--- server/test/controllers/page_controller_test.exs
+++ server/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Server.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Broccoli!"
   end
 end

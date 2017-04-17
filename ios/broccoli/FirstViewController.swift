e577c96b8f388e9d58c604cd4b30686f24ceb64c
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-04-17 18:49:13 +0200

    Adding the chartage dependency manager and adding Alamofire.

--- ios/broccoli/FirstViewController.swift
+++ ios/broccoli/FirstViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("http://192.168.178.206:4000/api/users").responseJSON { response in
+            print(response.request ?? "no request")  // original URL request
+            print(response.response ?? "no response") // HTTP URL response
+            print(response.data ?? "no data")     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {

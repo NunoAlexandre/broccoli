ab307e7786478dc416b32920c81180ffc03ad556
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-04-17 15:50:31 +0200

    Changed user_day code to to support json requests;

--- server/web/models/user.ex
+++ server/web/models/user.ex
@@ -1,10 +1,12 @@
 defmodule Server.User do
   use Server.Web, :model
+  import Doorman.Auth.Bcrypt, only: [hash_password: 1]
 
   schema "user" do
     field :name, :string
     field :email, :string
     field :hashed_password, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -14,8 +16,16 @@ defmodule Server.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :email, :hashed_password])
-    |> validate_required([:name, :email, :hashed_password])
+    |> cast(params, [:name, :email])
+    |> validate_required([:name, :email])
     |> unique_constraint(:email)
+  end
+
+  def create_changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> unique_constraint(:email)
+    |> hash_password
   end
 end

538465d244ad27d8f692aa94f16cc8e528304a54
Nuno Alexandre <nunombalexandre@gmail.com>
2017-04-29 18:11:07 +0200

    Cleaning phoenix warnings.

--- server/web/controllers/authentication_controller.ex
+++ server/web/controllers/authentication_controller.ex
@@ -1,7 +1,6 @@
 defmodule Server.AuthenticationController do
   use Server.Web, :controller
-  import Doorman.Login.Session, only: [login: 2]
-  alias Server.Authentication
+
 
   def index(conn, %{"credentials" => %{"email" => email, "password" => password}}) do
     authentication_response(conn, Doorman.authenticate(email, password))

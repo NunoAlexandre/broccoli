7064cc54b569495f574b8a9bb63903122e568e76
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-04-30 21:06:28 +0200

    Removing a button condition

--- ios/broccoli/UserDayFormViewController.swift
+++ ios/broccoli/UserDayFormViewController.swift
@@ -34,7 +34,6 @@ class UserDayFormViewController: FormViewController {
             <<< ButtonRow() { row in
                 row.title = "Save"
                 row.cell.height = {90}
-                row.disabled = Condition.function(["submission"], { form in form.allRows[0].isValid })
                 }
                 .onCellSelection {  cell, row in
                     let styled = DateFormatter()

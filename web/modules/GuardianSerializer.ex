7cee67c721deb1bc4dc6b8a1ce54f8442eb72170
Nuno Alexandre <nunombalexandre@gmail.com>
2017-05-06 23:11:20 +0200

    Adding AuthorizationTest and extracting the user id from the JWT Bearer subj.

--- web/modules/GuardianSerializer.ex
+++ web/modules/GuardianSerializer.ex
@@ -2,5 +2,6 @@ defmodule Broccoli.GuardianSerializer do
   @behaviour Guardian.Serializer
 
   def for_token(id), do: {:ok, id}
-  def from_token(id), do: {:ok, id}
+  def from_token("auth0|" <> userId), do: {:ok, userId}
+
 end

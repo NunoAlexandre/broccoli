4ae07f38f2d35c48659c7283fa4ed93741c3bb8d
Nuno Alexandre <nuno@channable.com>
2017-07-23 17:00:11 +0200

    Test level range and validation

--- test/controllers/user_day_controller_test.exs
+++ test/controllers/user_day_controller_test.exs
@@ -3,8 +3,8 @@ defmodule Broccoli.UserDayControllerTest do
 
   alias Broccoli.UserDay
 
-  @valid_attrs %{day: %{day: 7, month: 5, year: 2017}, level: :eight, note: "some content"}
-  @invalid_attrs %{day: %{day: 17, month: 4, year: 2010}, level: :seven, note: "some content"}
+  @valid_attrs %{day: %{day: 7, month: 5, year: 2017}, level: 3, note: "some content"}
+  @invalid_attrs %{day: %{day: 17, month: 4, year: 2010}, level: 8, note: "some content"}
   @valid_auth0_token "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJicm9jY29saS5uYWxleGFuZHJlLnRlc3QiLCJpYXQiOjE0OTQxMDQzOTIsImV4cCI6MzMwODI1NDkxOTIsImF1ZCI6InRlc3QiLCJzdWIiOiJhdXRoMHw1OTA4ZTQ3MThmY2I2MW5wMzg3ZTIzZjIifQ.urFUtu9x6dHH2A4UDAAefIgGZSk_2eTWGE-c1DYTEfs"
 
   setup %{conn: conn} do
@@ -25,7 +25,7 @@ defmodule Broccoli.UserDayControllerTest do
     conn = get conn, user_day_path(conn, :index)
     assert json_response(conn, 200)["data"] == [%{"id" => user_day.id,
                                                   "day" => to_string(user_day.day),
-                                                  "level" => to_string(user_day.level),
+                                                  "level" => user_day.level,
                                                   "note" => user_day.note,
                                                   "uid" => user_day.uid}]
   end
@@ -36,7 +36,7 @@ defmodule Broccoli.UserDayControllerTest do
     assert json_response(conn, 200)["data"] == %{ "id" => user_day.id,
                                                   "uid" => user_day.uid,
                                                   "day" => to_string(user_day.day),
-                                                  "level" => to_string(user_day.level),
+                                                  "level" => user_day.level,
                                                   "note" => user_day.note }
   end
 
--- test/models/user_day_test.exs
+++ test/models/user_day_test.exs
@@ -4,10 +4,20 @@ defmodule Broccoli.UserDayTest do
   alias Broccoli.UserDay
 
   @empty_attrs %{}
-  @valid_attrs %{uid: "1jkehfgrjdvb", day: %{day: 17, month: 4, year: 2010}, level: :eight, note: "some content"}
-  @invalid_attrs %{uid: 837438792, day: %{day: 17, month: 4, year: 2010}, level: :seven, note: "some content"}
+  @valid_attrs %{uid: "1jkehfgrjdvb", day: %{day: 17, month: 4, year: 2010}, level: 7, note: "some content"}
+  @invalid_attrs %{uid: 837438792, day: %{day: 17, month: 4, year: 2010}, level: 8, note: "some content"}
 
   test "changeset with valid attributes" do
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(1)).valid?
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(2)).valid?
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(3)).valid?
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(4)).valid?
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(5)).valid?
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(6)).valid?
+    assert UserDay.changeset(%UserDay{}, user_day_with_level(6)).valid?
+  end
+
+  test "changeset with valid level within range" do
     changeset = UserDay.changeset(%UserDay{}, @valid_attrs)
     assert changeset.valid?
   end
@@ -20,5 +30,23 @@ defmodule Broccoli.UserDayTest do
   test "changeset with incorrect uid and invalid level" do
     changeset = UserDay.changeset(%UserDay{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "changeset with level smaller than 1" do
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(0)).valid?
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(-1)).valid?
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(-2)).valid?
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(-3)).valid?
+  end
+
+  test "changeset with level bigger than 7" do
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(8)).valid?
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(9)).valid?
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(10)).valid?
+    refute UserDay.changeset(%UserDay{}, user_day_with_level(11)).valid?
+  end
+
+  def user_day_with_level(level) do
+    %{uid: "1jkehfgrjdvb", day: %{day: 17, month: 4, year: 2010}, level: level, note: "some content"}
   end
 end
